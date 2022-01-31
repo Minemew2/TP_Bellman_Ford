de6bd4399788942072b94a2a5f689734e1f22ac0
Nbrehin <80753480+Nbrehin@users.noreply.github.com>
2022-01-31 13:03:42 +0100

    Ajout de la méthode liste_precedent_tous(self):
    méthode qui renvoie une liste comprenant tous les index pointant vers un sommet et ce, pour tous les sommets.

--- bellman_ford.py
+++ bellman_ford.py
@@ -41,6 +41,24 @@ class Graphe:
                         list_prec.append(index_prec)
         return list_prec
     
+   
+     def liste_precedent_tous(self):
+         list_prec_tous=[]
+         for Sommet in self.sommets:
+             index_sommet = -1
+             index_prec = -1
+             list_prec = []
+             for i in self.sommets : 
+                 index_sommet +=1
+                 if i == Sommet :
+                     for x in range (0,len(self.sommets)):
+                         index_prec+=1
+                         if self.matrice[x][index_sommet] > 0:
+                             list_prec.append(index_prec)
+                         if x==len(self.sommets)-1:
+                             list_prec_tous.append(list_prec)
+         return list_prec_tous
+    
     
 
 
