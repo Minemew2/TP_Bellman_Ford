3d3d9cd41456ae354b9212d4158eccf9fbaad16e
Minemew2 <leanndre.mc2@gmail.com>
2022-02-18 15:33:29 +0100

    Avancement bellman

--- bellman_ford.py
+++ bellman_ford.py
@@ -18,53 +18,66 @@ class Graphe:
                         [0, 0, 0, 2, 11],
                         [0, 0, 0, 0, 3],
                         [0, 0, 0, 0, 0]]
-    def creer_tableau_bellman(self,debut,sommets):
+
+    def creer_tableau_bellman(self, debut, sommets):
         tableau = [[0, np.inf, np.inf, np.inf, np.inf],
                    [0, np.inf, np.inf, np.inf, np.inf],
                    [0, np.inf, np.inf, np.inf, np.inf],
                    [0, np.inf, np.inf, np.inf, np.inf]]
         return tableau
 
-    def 
     def bellman_ford(self, debut, sommets):
-        # tableau = []
-        # for x in range(len(self.sommets)-1):
-        # tableau.append([y for y in range(len(self.sommets))])
-        tableau = self.creer_tableau_bellman(debut,sommets)
-     
-    def liste_precedent(self,Sommet):
-        index_sommet = -1
-        index_prec = -1
-        list_prec = []
-        for i in self.sommets : 
-            index_sommet +=1
-            if i == Sommet :
-                for x in range (0,len(self.sommets)):
-                    index_prec+=1
-                    if self.matrice[x][index_sommet] > 0:
-                        list_prec.append(index_prec)
-        return list_prec
-    
-   
-     def liste_precedent_tous(self):
-         list_prec_tous=[]
-         for Sommet in self.sommets:
-             index_sommet = -1
-             index_prec = -1
-             list_prec = []
-             for i in self.sommets : 
-                 index_sommet +=1
-                 if i == Sommet :
-                     for x in range (0,len(self.sommets)):
-                         index_prec+=1
-                         if self.matrice[x][index_sommet] > 0:
-                             list_prec.append(index_prec)
-                         if x==len(self.sommets)-1:
-                             list_prec_tous.append(list_prec)
-         return list_prec_tous
-    
-    
+            # tableau = []
+            # for x in range(len(self.sommets)-1):
+            # tableau.append([y for y in range(len(self.sommets))])
+            tableau = self.creer_tableau_bellman(debut, sommets)
+            st = self.sommets[1]
+            tableau[1][1] = tableau[0][self.index_sommets(self.liste_precedent(st)[0])] + self.matrice[
+                self.index_sommets(self.liste_precedent(st)[0])][1]
 
+    def liste_precedent(self, Sommet):
+
+            index_sommet = -1
+
+            index_prec = -1
+            list_prec = []
+            for i in self.sommets:
+                index_sommet += 1
+                if i == Sommet:
+                    for x in range(0, len(self.sommets)):
+                        index_prec += 1
+                        if self.matrice[x][index_sommet] > 0:
+                            list_prec.append(index_prec)
+            return list_prec
+
+    def liste_precedent_tous(self):
+        list_prec_tous = []
+        for Sommet in self.sommets:
+            index_sommet = -1
+            index_prec = -1
+            list_prec = []
+            for i in self.sommets:
+                index_sommet += 1
+                if i == Sommet:
+                    for x in range(0, len(self.sommets)):
+                        index_prec += 1
+                        if self.matrice[x][index_sommet] > 0:
+                            list_prec.append(index_prec)
+                        if x == len(self.sommets) - 1:
+                            list_prec_tous.append(list_prec)
+        return list_prec_tous
+
+    def index_sommets(self, sommet):
+        if sommet == self.sommets[0]:
+            return 0
+        elif sommet == self.sommets[1]:
+            return 1
+        elif sommet == self.sommets[2]:
+            return 2
+        elif sommet == self.sommets[3]:
+            return 3
+        elif sommet == self.sommets[4]:
+            return 4
 
 
 if __name__ == "__main__":
