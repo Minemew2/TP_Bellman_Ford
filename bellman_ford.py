b638a030713d7ead9335b6c41904510aa4032470
Minemew2 <leandre.mc2@gmail.com>
2022-02-19 13:46:38 +0100

    algo belman fini, manque les posts conditions

--- bellman_ford.py
+++ bellman_ford.py
@@ -19,21 +19,43 @@ class Graphe:
                         [0, 0, 0, 0, 3],
                         [0, 0, 0, 0, 0]]
 
-    def creer_tableau_bellman(self, debut, sommets):
+    def creer_tableau_bellman(self, debut):
         tableau = [[0, np.inf, np.inf, np.inf, np.inf],
                    [0, np.inf, np.inf, np.inf, np.inf],
                    [0, np.inf, np.inf, np.inf, np.inf],
                    [0, np.inf, np.inf, np.inf, np.inf]]
         return tableau
 
-    def bellman_ford(self, debut, sommets):
+    def bellman_ford(self, debut):
             # tableau = []
             # for x in range(len(self.sommets)-1):
             # tableau.append([y for y in range(len(self.sommets))])
-            tableau = self.creer_tableau_bellman(debut, sommets)
-            st = self.sommets[1]
-            tableau[1][1] = tableau[0][self.index_sommets(self.liste_precedent(st)[0])] + self.matrice[
-                self.index_sommets(self.liste_precedent(st)[0])][1]
+        precedents = self.liste_precedent_tous()
+        origin_index = self.index_sommet(debut)
+
+        # TODO import numpy
+
+        tableau = self.creer_tableau_bellman(debut)
+
+        for ligne in range(1,len(self.sommets)-1):
+            for sommet in range(len(self.sommets)):
+                if self.sommets[sommet] != debut:
+                    distance_candidat = tableau[ligne-1][self.liste_precedent(self.sommets[sommet])[0]]+self.matrice[self.liste_precedent(self.sommets[sommet])[0]][sommet]
+                    for candidat in range(1,len(self.liste_precedent(self.sommets[sommet]))):
+                        distance_potentiel = tableau[ligne-1][self.liste_precedent(self.sommets[sommet])[candidat]]+self.matrice[self.liste_precedent(self.sommets[sommet])[candidat]][sommet]
+                        if distance_potentiel < distance_candidat:
+                            distance_candidat = distance_potentiel
+                    tableau[ligne][sommet] = distance_candidat
+
+        # for index_sommet in range(len(self.sommets)):
+            # for index_pre in precedents[index_sommet]:
+                # if line_n[index_sommet] - line_n[index_pre] >= self.matrice[index_pre][index_sommet]:
+                    # return "Belman ne s'applique pas au graphe"
+
+        return tableau[len(self.sommets)-2]
+    def liste_precedent_bouchon(self, sommet):
+        if sommet == self.sommets[0]:
+            return []
 
     def liste_precedent(self, Sommet):
 
@@ -67,7 +89,7 @@ class Graphe:
                             list_prec_tous.append(list_prec)
         return list_prec_tous
 
-    def index_sommets(self, sommet):
+    def index_sommet(self, sommet):
         if sommet == self.sommets[0]:
             return 0
         elif sommet == self.sommets[1]:
@@ -82,5 +104,4 @@ class Graphe:
 
 if __name__ == "__main__":
     g = Graphe()
-    a = Sommet("a")
-    print(g.bellman_ford(a))
+    print(g.bellman_ford(g.sommets[0]))
