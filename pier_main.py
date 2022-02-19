8d0c630607ee9efa6b376f0067e2c778e0041c50
Victor <castorvictor29@gmail.com>
2022-02-19 12:23:24 +0100

    pep8 + créations des premiers bouchons

--- pier_main.py
+++ pier_main.py
@@ -2,34 +2,35 @@
 
 class Sommet:
     def __init__(self, contenu=None):
-        self.visite=0
+        self.visite = 0
         self.contenu = contenu
 
+
 class Graphe:
-    def __init__(self, martice, sommets):
+    def __init__(self, matrice, sommets):
         self.matrice = matrice
         self.sommets = sommets
-        
+
     def liste_precedents(self, sommet):
         pass
 
     def liste_precedent_tous(self):
         pass
-    
+
     def bellman_ford(self, sommet_debut):
-        precedents = liste_precedents_tous()
-        origin_index = index_sommet(sommet_debut)
+        precedents = self.liste_precedents_tous()
+        origin_index = self.index_sommet(sommet_debut)
 
         # TODO import numpy
 
         line_n = [INFTY for i in range(len(self.sommets))]
         line_n[origin_index] = 0
-        line_n_plus = line_n # TODO deepcopy
+        line_n_plus = line_n  # TODO deepcopy
 
         # DO len(sommets)-1
-        for i in range(len(self.sommets)-1): # On fait l'opération sur N_SOMMETS-1 lignes.
-            for index_sommet in range(len(self.sommets)): # On itére a travers chaque sommet
-                for index_pre in precedents[index_sommet]: # On itére a travers les précédents du sommet
+        for i in range(len(self.sommets) - 1):  # On fait l'opération sur N_SOMMETS-1 lignes.
+            for index_sommet in range(len(self.sommets)):  # On itére a travers chaque sommet
+                for index_pre in precedents[index_sommet]:  # On itére a travers les précédents du sommet
                     if line_n_plus[index_sommet] > line_n[index_pre]:
                         line_n_plus[index_sommet] = line_n[index_pre]
             # TODO deepcopy
@@ -45,7 +46,11 @@ class Graphe:
     def index_sommet(sommet):
         retour = None
         for i in range(len(sommets)):
-            if sommets[i]==sommet:
-                retour=i
+            if sommets[i] == sommet:
+                retour = i
                 break
         return retour
+
+
+if __name__ == "__main__":
+    g = Graphe
